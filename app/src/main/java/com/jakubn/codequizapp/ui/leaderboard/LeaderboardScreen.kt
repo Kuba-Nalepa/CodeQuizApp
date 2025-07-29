12d14ed02ae20fe66fe23e2ae47ff06379163b5a
Kuba-Nalepa <nalepak11@gmail.com>
2025-07-29 17:06:37 +0200

    BottomSheet UI other user's profile in LeaderboardScreen

--- app/src/main/java/com/jakubn/codequizapp/ui/leaderboard/LeaderboardScreen.kt
+++ app/src/main/java/com/jakubn/codequizapp/ui/leaderboard/LeaderboardScreen.kt
@@ -3,6 +3,7 @@ package com.jakubn.codequizapp.ui.leaderboard
 import android.net.Uri
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -21,12 +22,18 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -42,12 +49,17 @@ import coil.compose.rememberAsyncImagePainter
 import com.jakubn.codequizapp.R
 import com.jakubn.codequizapp.model.CustomState
 import com.jakubn.codequizapp.model.User
+import com.jakubn.codequizapp.theme.Typography
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LeaderboardScreen(
     viewModel: LeaderboardViewModel = hiltViewModel()
 ) {
     val leaderboardState by viewModel.leaderboardUsers.collectAsState()
+    val sheetState = rememberModalBottomSheetState()
+
+    var selectedUser by remember { mutableStateOf<User?>(null) }
 
     Column(
         modifier = Modifier
@@ -72,6 +84,19 @@ fun LeaderboardScreen(
             modifier = Modifier.padding(bottom = 24.dp)
         )
 
+        if (selectedUser != null) {
+            ModalBottomSheet(
+                onDismissRequest = {
+                    selectedUser = null
+                },
+                sheetState = sheetState
+            ) {
+                selectedUser?.let { user ->
+                    UserMenuBottomSheet(user = user, inviteFriend = { }, textMessage = { }, challangePlayer = { }  )
+                }
+            }
+        }
+
         when (val state = leaderboardState) {
             is CustomState.Loading -> {
                 CircularProgressIndicator(
@@ -80,6 +105,7 @@ fun LeaderboardScreen(
                         .size(48.dp)
                 )
             }
+
             is CustomState.Success -> {
                 if (state.result.isEmpty()) {
                     Text(
@@ -94,11 +120,18 @@ fun LeaderboardScreen(
                         verticalArrangement = Arrangement.spacedBy(12.dp)
                     ) {
                         itemsIndexed(state.result) { index, user ->
-                            LeaderboardItem(position = index + 1, user = user)
+                            LeaderboardItem(
+                                position = index + 1,
+                                user = user,
+                                onCLick = {
+                                    selectedUser = user
+                                }
+                            )
                         }
                     }
                 }
             }
+
             is CustomState.Failure -> {
                 Text(
                     text = "Failed to load leaderboard: ${state.message}",
@@ -106,13 +139,14 @@ fun LeaderboardScreen(
                     modifier = Modifier.padding(16.dp)
                 )
             }
-            CustomState.Idle -> { }
+
+            CustomState.Idle -> {}
         }
     }
 }
 
 @Composable
-private fun LeaderboardItem(position: Int, user: User) {
+private fun LeaderboardItem(position: Int, user: User, onCLick: () -> Unit) {
     val goldColor = Color(0xFFFFD700)
     val silverColor = Color(0xFFC0C0C0)
     val bronzeColor = Color(0xFFCD7F32)
@@ -120,7 +154,10 @@ private fun LeaderboardItem(position: Int, user: User) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .height(72.dp),
+            .height(72.dp)
+            .clickable {
+                onCLick()
+            },
         colors = CardDefaults.cardColors(
             containerColor = MaterialTheme.colorScheme.surface.copy(
                 alpha = 0.8f
@@ -155,18 +192,21 @@ private fun LeaderboardItem(position: Int, user: User) {
                         tint = goldColor,
                         modifier = Modifier.size(28.dp)
                     )
+
                     2 -> Icon(
                         painter = painterResource(R.drawable.ic_second_place),
                         contentDescription = "Second place",
                         tint = silverColor,
                         modifier = Modifier.size(28.dp)
                     )
+
                     3 -> Icon(
                         painter = painterResource(R.drawable.ic_third_place),
                         contentDescription = "Third place",
                         tint = bronzeColor,
                         modifier = Modifier.size(28.dp)
                     )
+
                     else -> Spacer(modifier = Modifier.size(28.dp))
                 }
             }
@@ -214,6 +254,75 @@ private fun LeaderboardItem(position: Int, user: User) {
                 color = Color.Black,
                 textAlign = TextAlign.End
             )
+        }
+    }
+}
+
+@Composable
+private fun UserMenuBottomSheet(user: User, inviteFriend: () -> Unit, textMessage: () -> Unit, challangePlayer: () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(Uri.parse(user.imageUri)),
+            contentDescription = "Profile Picture",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(120.dp)
+                .clip(CircleShape)
+        )
+
+        user.name?.let { username ->
+            Text(
+                modifier = Modifier.padding(top = 10.dp),
+                text = username,
+                style = Typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+        }
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(20.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.spacedBy(5.dp)) {
+                Icon(
+                    modifier = Modifier.size(40.dp).clip(RoundedCornerShape(100)).clickable {
+                        inviteFriend()
+                    }.padding(5.dp),
+                    painter = painterResource(R.drawable.ic_person_add),
+                    tint = Color.Black,
+                    contentDescription = "Add friend"
+                )
+                Text(style = Typography.labelSmall, text = "Add friend")
+            }
+            Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.spacedBy(5.dp)) {
+                Icon(
+                    modifier = Modifier.size(40.dp).clip(RoundedCornerShape(100)).clickable {
+                        textMessage()
+                    }.padding(5.dp),
+                    painter = painterResource(R.drawable.ic_message),
+                    tint = Color.Black,
+                    contentDescription = "Text a message"
+                )
+                Text(style = Typography.labelSmall, text = "Text a message")
+            }
+            Column(horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.spacedBy(5.dp)) {
+                Icon(
+                    modifier = Modifier.size(40.dp).clip(RoundedCornerShape(100)).clickable {
+                        challangePlayer()
+                    }.padding(5.dp),
+                    painter = painterResource(R.drawable.ic_play),
+                    tint = Color.Black,
+                    contentDescription = "Challenge the player"
+                )
+                Text(style = Typography.labelSmall, text = "Challenge")
+            }
         }
     }
 }
